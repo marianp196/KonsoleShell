5b4241400c711e1f3c471e08191c358715743f7e
marianp <marianpatzke@gmail.com>
2018-01-07 16:44:49 +0100

    initial Input

---
+++ src/konsoleninputtest/KonsoleInput.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package konsoleninputtest;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author marian
+ */
+public class KonsoleInput{
+
+    public KonsoleInput(IKonsoleInputAction consoleInputAction) {
+        if(consoleInputAction == null)
+            throw new NullPointerException("consoleInputAction");
+        this.consoleInputAction = consoleInputAction;
+        thread = new Thread(new ConsoleInputThread(consoleInputAction));
+        thread.start();
+    }
+           
+    private IKonsoleInputAction consoleInputAction;
+    private Thread thread;   
+    
+    private class ConsoleInputThread implements Runnable{
+        public ConsoleInputThread(IKonsoleInputAction consoleInputAction) {
+            this.consoleInputAction = consoleInputAction;
+        }
+        
+         @Override
+        public void run() 
+        {
+             while(true)
+            {
+                try {
+                    byte[] b = new byte[100];
+                    int i = System.in.read(b);
+                    String s = new String(b,0,i);
+                    consoleInputAction.ActionConsoleInput(s);
+                } catch (IOException ex) {
+                    Logger.getLogger(KonsolenInputTest.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        
+        private IKonsoleInputAction consoleInputAction;       
+    }
+}
---
+++ src/konsoleninputtest/KonsolenInputTest.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package konsoleninputtest;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author marian
+ */
+class KonsolenInputTest implements IKonsoleInputAction{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {   
+        KonsoleInput ci = new KonsoleInput(new KonsolenInputTest());
+    }
+
+    @Override
+    public void ActionConsoleInput(String inputText) 
+    {
+        System.out.print(inputText);
+    }
+}
